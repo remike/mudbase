6ddfd2dbf34634209230e9eea50cc3936491141d
Mike Red <t_mihai91@yahoo.com>
2010-04-24 20:39:06 +0300

    Putty problems fixed. Works great. ID assignment, host recognition and listing. Possibly abstract enough already, needs refactoring.

--- main.py
+++ main.py
@@ -1,20 +1,28 @@
 from twisted.internet.protocol import ServerFactory, Protocol
-from twisted.conch.telnet import StatefulTelnetProtocol
+from twisted.conch.telnet import StatefulTelnetProtocol, TelnetTransport
 from twisted.internet import reactor
+from twisted.application.internet import TCPServer
+from twisted.application.service import Application
 
 from time import sleep
+
 
 # factory - protocol class
 # protocol/factory -> network class
 # network -> game class
-# Requests and callbacks go down in the list.
-# 
+# Requests and callbacks go down in the list. 
 
 class NetworkClass():
+
+	
+	commands = ["help", "exit", "go north", "hosts" ]
+
 	def __init__(self):
 		print "Starting up network server.."
+		#self.protocol is the TYPE of protocol which gets created
+		#it's only the function which gets called, not the actual protocol
 		self.child = ServerFactory()
-		self.child.protocol = MyProtocol
+		self.child.protocol = lambda: TelnetTransport(MyProtocol)
 		self.protocol = self.child.protocol
 		self.child.id = 1
 		self.child.clList = { }
@@ -25,18 +33,36 @@ class NetworkClass():
 		self.clList[id].transport.loseConnection()
 	def sendLine(self,line,id):
 		self.clList[id].sendLine(line)
+	def sendData(self,data,id):
+		self.clList[id].transport.write(data)
 	def lineReceived(self,line,id):
-		if line != "exit":
-			if line[0]!="@":
-				self.clList[id].sendLine("local: " + line)
+		if line not in self.commands:
+			if line.startswith("@") == False:
+				self.clList[id].sendLine("Unrecognised command. Try 'help'?")
 			else:
 				for cl in self.clList:
 					if cl != id:
-						self.clList[cl].sendLine("GLOBAL( "+id+" ): " + line)	
+						self.clList[cl].sendLine("GLOBAL( "+str(id)+" ): " + line[1:])	
+			self.sendData("> ",id)
 		else:
-			self.clList[id].sendLine("Killed.")
-			print "Client killed."
-			self.disconnect(id)
+			if line == "exit":
+				self.clList[id].sendLine("Killed.")
+				print "Client killed."
+				self.disconnect(id)
+			elif line == "help":
+				self.sendLine("Welcome.",id)
+				self.sendLine("'@message' will broadcast 'message' globally to all connected clients.",id)
+				self.sendLine("The command 'hosts' will list all connected clients and their ips.",id)
+				self.sendLine("You can quit with 'exit'",id)
+			elif line == "hosts":
+				for cl in self.clList:
+					x = str(cl) + " - "
+					x += str(self.clList[cl].transport.getPeer())
+					self.sendLine(x,id)
+				self.sendLine("----",id)
+			elif line == "go north":
+				self.sendLine("Sorry, not yet. ;)",id)
+			self.sendData("> ",id)
 
 class MyProtocol(StatefulTelnetProtocol):
 	def connectionMade(self):
@@ -44,6 +70,7 @@ class MyProtocol(StatefulTelnetProtocol):
 		self.sendLine("Welcome.")
 		self.sendLine("Do '@message' to output message to all connected clients.")
 		self.sendLine("'exit' to quit.")
+		self.transport.write("> ")
 		self.clearLineBuffer()
 		self.id = self.factory.id
 		self.factory.id+=1
@@ -51,11 +78,21 @@ class MyProtocol(StatefulTelnetProtocol):
 		self.parent = self.factory.parent
 	def lineReceived(self,line):
 		print "Message received: " + line
-		print self.id
 		self.parent.lineReceived(line.strip(),self.id)
 		self.clearLineBuffer()
 	def connectionLost(self,reason):
 		print "Connection lost."
+		del self.factory.clList[self.id]
+	
+	def enableRemote(self, option):
+		return False
+	def disableRemote(self, option):
+		return False
+	def enableLocal(self,option):
+		return False
+	def disableLocal(self,option):
+		return False
+
 
 
 if __name__=="__main__":
